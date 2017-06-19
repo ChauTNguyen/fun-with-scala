055e087010463b88f3f24d7bd682d1b956c2f79f
Chau Nguyen <chautnguyen96@gmail.com>
2017-06-18 20:48:54 -0700

    Add ex4 currying

--- src/main/scala-2.12/gettingstarted/ex3.scala
+++ src/main/scala-2.12/gettingstarted/ex3.scala
@@ -3,6 +3,7 @@ package gettingstarted
 class ex3 {
   def partial1[A,B,C](a: A, f: (A,B) => C): B => C
     = f(a, _)
+
   def partial1Long[A,B,C](a: A, f: (A,B) => C): B => C
     = (b: B) => f(a, b)
 }
